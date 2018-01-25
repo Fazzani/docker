4c785fe12ec58945b5a2d6f9d7410e8e8e19add2
Fazzani <tunisienheni@outlook.com>
2018-01-25 23:54:58 +0100

    fixs

--- stack elk/scripts/create-swarm.sh
+++ stack elk/scripts/create-swarm.sh
@@ -8,39 +8,40 @@ read -p "Master node count (default=1)" -t 10 -i "1" master_node_count
 
 # Creating 6 nodes 
 echo "### Creating nodes ..."
-for c in $(seq 1 $((total_node_count))) ; do
+for c in $(seq 1 $((total_node_count))); do
     echo "Creating not $prefix_node_name_$c with driver $driver"
-    # docker-machine create -d $driver $prefix_node_name_$c
+    docker-machine create -d $driver $prefix_node_name_$c
 done
 
-# # Get IP from leader node
-# leader_ip=$(docker-machine ip $prefix_node_name_$1)
+# Get IP from leader node
+leader_ip=$(docker-machine ip $prefix_node_name_1)
+echo "Leader node IP : $leader_ip"
 
-# # Init Docker Swarm mode
-# echo "### Initializing Swarm mode ..."
-# eval $(docker-machine env node1)
-# docker swarm init --advertise-addr $leader_ip
+# Init Docker Swarm mode
+echo "### Initializing Swarm mode ..."
+eval $(docker-machine env prefix_node_name_1)
+docker swarm init --advertise-addr $leader_ip
 
-# # Swarm tokens
-# manager_token=$(docker swarm join-token manager -q)
-# worker_token=$(docker swarm join-token worker -q)
+# Swarm tokens
+manager_token=$(docker swarm join-token manager -q)
+worker_token=$(docker swarm join-token worker -q)
 
-# # Joinig manager nodes
-# echo "### Joining manager modes ..."
-# for c in {2..$((master_node_count + 2))} ; do
-#     eval $(docker-machine env $prefix_node_name_$c)
-#     docker swarm join --token $manager_token $leader_ip:2377
-# done
+# Joinig manager nodes
+echo "### Joining manager $master_node_count modes ..."
+for c in $(seq 2 $((master_node_count))); do
+    eval $(docker-machine env $prefix_node_name_$c
+    docker swarm join --token $manager_token $leader_ip:2377
+done
 
-# # Join worker nodes
-# echo "### Joining worker modes ..."
-# for c in {4..6} ; do
-#     eval $(docker-machine env $prefix_node_name_$c)
-#     docker swarm join --token $worker_token $leader_ip:2377
-# done
+# Join worker nodes
+echo "### Joining worker modes ..."
+for c in $(seq $master_node_count $total_node_count); do
+    eval $(docker-machine env $prefix_node_name_$c)
+    docker swarm join --token $worker_token $leader_ip:2377
+done
 
-# # Clean Docker client environment
-# echo "### Cleaning Docker client environment ..."
-# eval $(docker-machine env -u)
+# Clean Docker client environment
+echo "### Cleaning Docker client environment ..."
+eval $(docker-machine env -u)
 
-# exit 0
+exit 0
