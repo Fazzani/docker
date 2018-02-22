c4d4ff76f78e51722221d3fc3ea60cb40dd892d2
Fazzani <tunisienheni@outlook.com>
2018-02-23 00:01:01 +0100

    fixs

--- webgrab/config/webgrab-docker.sh
+++ webgrab/config/webgrab-docker.sh
@@ -9,8 +9,8 @@ cd /config
 
 for webGrab in ./*.config.xml; do
   echo "Processing webGrab file => $webGrab"
-  echo "Moving $WebGrab to WebGrab++.config.xml"
-  mv -u $WebGrab "$configDir/WebGrab++.config.xml"
+  echo "Moving $webGrab to WebGrab++.config.xml"
+  mv -u $webGrab "$configDir/WebGrab++.config.xml"
   echo "Launching webgrab"
   wget https://api.pushover.net/1/messages.json --post-data="token=a1zc9d81aw14ezws414n7uvsnz2xio&user=uxepp2gjx5ch4eveufj8fo8jmcm6we&device=sm-g935f&title=WebGrabber+message&message=WebGrabber+extrating+xmltv+file+launched.+Working+directory+:+$webGrab"  -qO- > /dev/null 2>>4 &
   mono "$webgrabDir/bin/WebGrab+Plus.exe" $configDir >> $logfilename
