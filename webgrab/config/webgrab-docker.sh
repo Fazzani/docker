491ea758aedb9d57d3f8c93b42c73a8112adcb75
Fazzani <tunisienheni@outlook.com>
2018-02-23 00:39:53 +0100

    fix

--- webgrab/config/webgrab-docker.sh
+++ webgrab/config/webgrab-docker.sh
@@ -23,7 +23,7 @@ for webGrab in ./*.config.xml; do
   mono "$webgrabDir/bin/WebGrab+Plus.exe" $configDir >> $logfilename 
   # message=$(tail -n2 $logfilename) && \
   # sudo wget https://api.pushover.net/1/messages.json --post-data="token=a1zc9d81aw14ezws414n7uvsnz2xio&user=uxepp2gjx5ch4eveufj8fo8jmcm6we&device=sm-g935f&title=WebGrabber+message&message=$message."  -qO- && \
-  sleep 5
+  #sleep 5
 done
 
 exit 0
