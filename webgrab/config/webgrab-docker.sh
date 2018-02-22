0788c8942b87d4b9fd15ba409c9b0c0c6905d08e
Fazzani <tunisienheni@outlook.com>
2018-02-23 00:06:19 +0100

    fixs

--- webgrab/config/webgrab-docker.sh
+++ webgrab/config/webgrab-docker.sh
@@ -12,10 +12,10 @@ for webGrab in ./*.config.xml; do
   echo "Moving $webGrab to WebGrab++.config.xml"
   mv -u $webGrab "$configDir/WebGrab++.config.xml"
   echo "Launching webgrab"
-  wget https://api.pushover.net/1/messages.json --post-data="token=a1zc9d81aw14ezws414n7uvsnz2xio&user=uxepp2gjx5ch4eveufj8fo8jmcm6we&device=sm-g935f&title=WebGrabber+message&message=WebGrabber+extrating+xmltv+file+launched.+Working+directory+:+$webGrab"  -qO- > /dev/null 2>>4 &
-  mono "$webgrabDir/bin/WebGrab+Plus.exe" $configDir >> $logfilename
-  message=$(tail -n2 $logfilename)
-  sudo wget https://api.pushover.net/1/messages.json --post-data="token=a1zc9d81aw14ezws414n7uvsnz2xio&user=uxepp2gjx5ch4eveufj8fo8jmcm6we&device=sm-g935f&title=WebGrabber+message&message=$message."  -qO-  > /dev/null 2>>4
+  wget https://api.pushover.net/1/messages.json --post-data="token=a1zc9d81aw14ezws414n7uvsnz2xio&user=uxepp2gjx5ch4eveufj8fo8jmcm6we&device=sm-g935f&title=WebGrabber+message&message=WebGrabber+extrating+xmltv+file+launched.+Working+directory+:+$webGrab"  -qO- > && \
+  mono "$webgrabDir/bin/WebGrab+Plus.exe" $configDir >> $logfilename && \
+  message=$(tail -n2 $logfilename) && \
+  sudo wget https://api.pushover.net/1/messages.json --post-data="token=a1zc9d81aw14ezws414n7uvsnz2xio&user=uxepp2gjx5ch4eveufj8fo8jmcm6we&device=sm-g935f&title=WebGrabber+message&message=$message."  -qO- && \
   sleep 5
 done
 
