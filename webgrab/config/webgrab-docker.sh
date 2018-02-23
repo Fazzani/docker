0a31b2351e37ab9f0c5f6180745553bd87968746
Fazzani <tunisienheni@outlook.com>
2018-02-23 20:19:51 +0100

    version 0.2 webgrab

--- webgrab/config/webgrab-docker.sh
+++ webgrab/config/webgrab-docker.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -euox
+
 #---------------------------
 # Variables
 #---------------------------
@@ -8,6 +10,7 @@ configDir="/config"
 logfilename="webgrab.log"
 repo_git_name="webgrab-configs"
 repo_git_url="https://github.com/Fazzani/webgrab-configs.git"
+
 #---------------------------
 # Install all needed tools
 #---------------------------
@@ -27,11 +30,15 @@ function install_necessary_packages {
 function push_to_git
 {
   # coping output file to git folder
-  cp -f guide.tar.gz ./$repo_git_name/ && \
+  cp -f guide.tar.gz ./$repo_git_name/
   echo "push to git"
-  cd $1 && \
-  git push && \
-  cd .. 
+  cd $1
+
+  git add --all
+  now=$(date +"%c")
+  git commit -m "new guide $now"
+  git push
+  cd ..
   return 0
 }
 
@@ -42,15 +49,15 @@ function push_to_git
 #--------------------------
 function latest_from_git
 {
-  echo "Getting latest version from repo git: $1" 
+  echo "Getting latest version from repo git: $1 will be founded in $2" 
   if [ ! -d .git ]; then
     echo "The repo $1 not exist so we clone it"
     git clone $1
   fi
 
-  cd $2 && \
-  git pull && \
-  cp -f *.config.xml ../ && \
+  cd $2
+  git pull
+  cp -f *.config.xml ../
   cd ..
   return 0
 }
@@ -64,21 +71,25 @@ latest_from_git $repo_git_url $repo_git_name
 
 for webGrab in ./*.config.xml; do
 
-  echo "Processing webGrab file => $webGrab"
+  echo "Processing webgrab file => $webGrab"
   echo "Moving $webGrab to WebGrab++.config.xml"
   cp -f $webGrab "$configDir/WebGrab++.config.xml"
 
   echo "Launching webgrab"
-  wget https://api.pushover.net/1/messages.json --post-data="token=a1zc9d81aw14ezws414n7uvsnz2xio&user=uxepp2gjx5ch4eveufj8fo8jmcm6we&device=sm-g935f&title=WebGrabber+message&message=WebGrabber+extrating+xmltv+file+launched.+Working+directory+:+$webGrab"  -qO- && \
+  wget https://api.pushover.net/1/messages.json \
+    --post-data="token=a1zc9d81aw14ezws414n7uvsnz2xio&user=uxepp2gjx5ch4eveufj8fo8jmcm6we&device=sm-g935f&title=WebGrabber+message&message=WebGrabber+extrating+xmltv+file+launched.+Working+directory+:+$webGrab" \
+    -qO-
   mono "$webgrabDir/bin/WebGrab+Plus.exe" $configDir
-  message=$(tail -n2 $logfilename) && \
-  wget https://api.pushover.net/1/messages.json --post-data="token=a1zc9d81aw14ezws414n7uvsnz2xio&user=uxepp2gjx5ch4eveufj8fo8jmcm6we&device=sm-g935f&title=WebGrabber+message&message=$message."  -qO-
+  message=$(tail -n2 $logfilename)
+  wget https://api.pushover.net/1/messages.json \
+    --post-data="token=a1zc9d81aw14ezws414n7uvsnz2xio&user=uxepp2gjx5ch4eveufj8fo8jmcm6we&device=sm-g935f&title=WebGrabber+message&message=$message." \
+   -qO-
 
 done
 
-echo "End of grabbing" && \
-echo "Compressing all xmltv" && \
-tar -czf guide.tar.gz *.xmltv && \
+echo "End of grabbing"
+echo "Compressing all xmltv"
+tar -czf guide.tar.gz *.xmltv
 
 #pushing to git
 push_to_git $repo_git_url 
